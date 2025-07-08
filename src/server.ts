d08ada26f453cfc119767a760534795f2bc7c14e
leticea <nascimento.leticeah@gmail.com>
2025-07-07 22:54:56 -0300

    feat(get-rooms.ts): creating get rooms route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
-import { sql } from "./db/connection.ts";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -21,4 +21,5 @@ app.get("/health", () => {
   return "OK";
 });
 
+app.register(getRoomsRoute);
 app.listen({ port: env.PORT });
