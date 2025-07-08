07febc59f431ca1f36f56f53b4414007ceb612c2
leticea <nascimento.leticeah@gmail.com>
2025-07-08 13:53:58 -0300

    feat(get-rooms.ts) database configurations

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -5,9 +5,13 @@ import { schema } from "../../db/schema/index.ts";
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get("/rooms", async () => {
     const results = await db
-      .select()
+      .select({
+        id: schema.rooms.id,
+        name: schema.rooms.name,
+      })
       .from(schema.rooms)
       .orderBy(schema.rooms.createdAt);
+
     return results;
   });
 };
