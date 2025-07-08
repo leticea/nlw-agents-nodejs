87cd3186cacd60f48d316f97ada4d6b471b621e7
leticea <nascimento.leticeah@gmail.com>
2025-07-07 21:43:44 -0300

    feat(docker-compose.yml): docker config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
+import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
-import { fastifyCors } from "@fastify/cors";
 import { env } from "./env.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -20,7 +20,4 @@ app.get("/health", () => {
   return "OK";
 });
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log(`Port: ${process.env.PORT}`);
-  console.log("HTTP server running!");
-});
+app.listen({ port: env.PORT });
