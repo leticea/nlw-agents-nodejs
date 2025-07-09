7af7e5166d541a514153e692e3db0c5620ac8f15
leticea <nascimento.leticeah@gmail.com>
2025-07-09 20:15:38 -0300

    feat: creating get room questions route

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import {
 import { env } from "./env.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 import { createRoomRoute } from "./http/routes/create-room.ts";
+import { getRoomQuestions } from "./http/routes/get-room-questions.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -24,5 +25,6 @@ app.get("/health", () => {
 
 app.register(getRoomsRoute);
 app.register(createRoomRoute);
+app.register(getRoomQuestions);
 
 app.listen({ port: env.PORT });
